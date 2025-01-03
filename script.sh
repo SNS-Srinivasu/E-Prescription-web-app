adb163dfb77644ecd45020c499307c17ba63b9ce
Sukuru_Naga_Sai_Srinivasu <saipandu81@gmail.com>
2025-01-03 12:15:26 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#Update Package Repositories
+sudo apt-get update -y
+
+#Install Node.js 
+sudo apt-get install -y nodejs 
+
+#Install SQLite3
+sudo apt-get install -y sqlite3
+
+#Install Git 
+sudo apt-get install git -y
+
