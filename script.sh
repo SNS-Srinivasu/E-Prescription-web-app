45a1573e3b172706adc1d9cd2427efccc5f3f5fa
Sukuru_Naga_Sai_Srinivasu <saipandu81@gmail.com>
2025-01-03 13:28:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 
-#Update Package Repositories
+
+# Update Package Repositories
 sudo apt-get update -y
 
-#Install Node.js 
-sudo apt-get install -y nodejs 
+# Install Node.js 
+sudo apt-get install -y nodejs
 
-#Install SQLite3
+# Install SQLite3
 sudo apt-get install -y sqlite3
 
-#Install Git 
-sudo apt-get install git -y
+# Install Git
+sudo apt-get install -y git
 
+# Install npm 
+sudo apt-get install -y npm
+
+# Install required Node.js modules
+npm install express body-parser sqlite3
